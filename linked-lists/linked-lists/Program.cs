da1fe7f4a054c5e4c995dc60e1411fb0a4710312
Thomas Reyes <thomasellison.r@gmail.com>
2017-09-22 20:35:43 -0700

    challenge finished

---
+++ linked-lists/linked-lists/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList potatoList = new LinkedList(1);
+
+            potatoList.AddNode(2);
+            potatoList.AddNode(3);
+            potatoList.AddNode(4);
+            potatoList.AddToFirst(0);
+            potatoList.ViewLinkedList();
+            Console.Read();
+        }
+    }
+}
